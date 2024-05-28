f2df5273875b8473e350feb31827f2fc90864e23
Siponik <semenpopov505@gmail.com>
2024-05-29 00:45:38 +0400

    held mini test, added comments

--- Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyInstance.cs
+++ Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyInstance.cs
@@ -32,13 +32,15 @@ public class EnemyInstance: MonoBehaviour
         Physics.OverlapSphereNonAlloc(transform.position, _viewRadius, _noticedTarget, _layerMaskPlayer);
         
         //добавить проверку на то что бы нельзя было входить в одно состояние несколько раз
+        //да, нихуя не работает еще и ломается все, состояние дрочится
         if (_noticedTarget[0] == null)
         {
-            _enemyStateMachine.EnterIn<PatrolEnemyState>();
+            //_enemyStateMachine.EnterIn<PatrolEnemyState>();
             _isPlayerVisible = false;
             return;
         }
-        
+        //досюда даже не доходит без переключения, не обрабатывает от иф сверху
+        Debug.Log("player is sphere");
         _target = _noticedTarget[0].transform;
         Vector3 playerPosition = _target.position;
         Vector3 direction = playerPosition  - transform.position;
