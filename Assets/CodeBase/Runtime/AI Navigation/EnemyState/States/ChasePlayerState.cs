d48cbbcf076149479866742c5e7410fcb1534a68
Siponik <semenpopov505@gmail.com>
2024-05-29 00:35:21 +0400

    Added chaseEnemyState
    not tested

--- Assets/CodeBase/Runtime/AI Navigation/EnemyState/States/ChasePlayerState.cs
+++ Assets/CodeBase/Runtime/AI Navigation/EnemyState/States/ChasePlayerState.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public class ChasePlayer : MonoBehaviour, IEnemyState
+public class ChasePlayerState : IEnemyState
 {
+    private readonly EnemyInstance _enemyInstance;
     private readonly EnemyStateMachine _enemyStateMachine;
+    private Transform _targetEnemy;
+    private bool _isPlayerVisible;
+    private float _viewAngle;
+    private float _vievRadius;
 
-    public ChasePlayer(EnemyStateMachine enemyStateMachine)
+
+    public ChasePlayerState(EnemyStateMachine enemyStateMachine, EnemyInstance enemyInstance)
     {
         _enemyStateMachine = enemyStateMachine;
+        _enemyInstance = enemyInstance;
     }
 
     public void EnterState()
     {
-        
+        ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        NavMeshAgent navMeshAgent = _enemyStateMachine.NavMeshAgent;
+        Vector3 targetEnemyPosition = _enemyStateMachine.TargetEnemy.position;
+
+        Vector3 playerPosition = targetEnemyPosition;
+
+        navMeshAgent.destination = targetEnemyPosition;
     }
 
     public void ExitState()
