0a37c2d894ac0fcc1953fdcde3a5a52828fc9cc3
VolumeBlez <dchinicalov@gmail.com>
2024-05-29 15:16:23 +0300

    update Enemy AI from tttllliiinnn

--- Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyInstance.cs
+++ Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyInstance.cs
@@ -8,21 +8,55 @@ public class EnemyInstance: MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
     private EnemyStateMachine _enemyStateMachine;
-    
-    [SerializeField] private NavMeshSurface _surface;
+    private Transform _target;
     [SerializeField] private float _viewRadius = 6f;
     
-    
+    [SerializeField] private NavMeshSurface _surface;
+    private Collider[] _noticedTarget = new Collider[1];
+    [SerializeField] private LayerMask _layerMaskPlayer;
+    private double _viewAngle;
+    private bool _isPlayerVisible;
+
     private void Awake()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        _enemyStateMachine = new EnemyStateMachine(_navMeshAgent, this);
+        _enemyStateMachine = new EnemyStateMachine(_navMeshAgent, _target, this);
+        
         _surface.BuildNavMesh();
+        
         _enemyStateMachine.EnterIn<PatrolEnemyState>();
     }
-    // private void OnDrawGizmosSelected()
-    // {
-    //     Gizmos.color = Color.red;
-    //     Gizmos.DrawSphere(transform.position, _viewRadius);
-    // }
+
+    private void Update()
+    {
+        Physics.OverlapSphereNonAlloc(transform.position, _viewRadius, _noticedTarget, _layerMaskPlayer);
+        
+        //добавить проверку на то что бы нельзя было входить в одно состояние несколько раз
+        //да, нихуя не работает еще и ломается все, состояние дрочится
+        if (_noticedTarget[0] == null)
+        {
+            //_enemyStateMachine.EnterIn<PatrolEnemyState>();
+            _isPlayerVisible = false;
+            return;
+        }
+        //досюда даже не доходит без переключения, не обрабатывает от иф сверху
+        Debug.Log("player is sphere");
+        _target = _noticedTarget[0].transform;
+        Vector3 playerPosition = _target.position;
+        Vector3 direction = playerPosition  - transform.position;
+        
+        float angle = Vector3.Angle(_enemyStateMachine.TargetEnemy.forward, direction);
+        
+        _isPlayerVisible = angle < _viewAngle;
+
+        if (_isPlayerVisible)
+        {
+            _enemyStateMachine.EnterIn<ChasePlayerState>();
+        }
+        else
+        {
+            _enemyStateMachine.EnterIn<PatrolEnemyState>();
+        }
+        
+    }
 }
--- Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyStateMachine.cs
+++ Assets/CodeBase/Runtime/AI Navigation/EnemyState/EnemyStateMachine.cs
@@ -6,17 +6,20 @@ using UnityEngine.AI;
 public class EnemyStateMachine
 {
     public NavMeshAgent NavMeshAgent { get; }
+    public Transform TargetEnemy { get; }
+    
     private Dictionary<Type, IEnemyState> _enemyStates;
     private IEnemyState _currentEnemyState;
     
-    public EnemyStateMachine(NavMeshAgent navMeshAgent, EnemyInstance enemyInstance)
+    public EnemyStateMachine(NavMeshAgent navMeshAgent,Transform targetEnemy, EnemyInstance enemyInstance)
     {
         NavMeshAgent = navMeshAgent;
+        TargetEnemy = targetEnemy;
         
         _enemyStates = new Dictionary<Type, IEnemyState>()
         {
             [typeof(PatrolEnemyState)] = new PatrolEnemyState(this, enemyInstance),
-            [typeof(ChasePlayer)] = new ChasePlayer(this),
+            [typeof(ChasePlayerState)] = new ChasePlayerState(this, enemyInstance),
             [typeof(AttackPlayer)] = new AttackPlayer(this)
         };
     }
